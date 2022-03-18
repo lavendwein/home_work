3b052015ce8159bf2a91bacb4e9fa099327e0b1a
Polina Popova <pollinap23@yandex.ru>
2022-03-18 22:18:35 +0500

    home_work

---
+++ homework.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <fstream>
+#include <list>
+#include <iterator>
+//#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    list<double>numbers;
+    ifstream file("file.txt");
+    if (!file) {
+        cout << "File not found!" << endl;
+        return -1;
+    }
+    while (!file.eof()) {
+        double num;
+        file >> num;
+        numbers.push_back(num);
+    }
+    numbers.sort();
+    double sum = 0;
+    for (double i : numbers) {
+        cout << i << endl;
+        sum += i;
+    }
+    cout << sum / numbers.size() << endl;
+}
---
+++ homework2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int main()
+{
+    list <int> numbers;
+
+    int sum = 0;
+    for (int i = 0; i < 10000; i++) {
+        int number = rand() % 201 - 100;
+        sum += number;
+        numbers.push_back(number);
+    }
+    cout << sum / 10000.0 << endl;
+    return 0;
+}
